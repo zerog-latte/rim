4969a12e08e6d53f03479a8c13f150cc2dd8a946
Andrew Sorokin <andrewsorokinch@gmail.com>
2023-11-10 19:11:01 +0300

    Add get Items request

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,16 +1,21 @@
 <template>
-<div>
-  <div v-for="(category, key) in data" :key="key">
-    <div>
-      <div v-if="category.subcats && !category.parent">
-        <h3 class="cap">{{category._id}}</h3>
-      </div>
-      <div v-else>
-        <span class="cap">{{category._id}}</span> - {{category.desc}}
+  <div>
+    <div v-for="(category, key) in data" :key="key">
+      <div>
+        <div v-if="category.subcats && !category.parent">
+          <h3 class="cap">{{ category._id }}</h3>
+        </div>
+        <div v-else>
+          <h5><span class="cap">{{ category._id }}</span> - {{ category.desc }}</h5>
+          <ul v-if="category.items">
+            <li v-for="(item, index) in category.items" :key="index">
+              {{ item.desc }}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
@@ -34,10 +39,23 @@ export default {
       axios.get('/categories')
         .then((res) => {
           this.data = res.data;
+          this.getCategoryContent();
         })
         .catch((error) => {
           console.error(error);
         });
+    },
+    async getCategoryContent() {
+      for (const cat of this.data) {
+        await axios
+          .get(`/items/${cat._id}`)
+          .then((res) => {
+            cat.items = res.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
     },
   },
   created() {
